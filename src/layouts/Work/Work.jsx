ab53cae75e372bef22b3cc618a9c49111b101fc9
matheuslimaandrade <matheusla2f04@gmail.com>
2023-12-13 18:13:26 -0300

    work component

---
+++ src/layouts/Work/Work.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { Project } from "../../components/Project/Project";
+
+export default function Work() {
+    return(
+        <>
+        <div className="grid grid-cols-1">
+            <Project></Project>
+            <Project></Project>
+            <Project></Project>
+            <Project></Project>
+        </div>
+        </>
+    )
+}

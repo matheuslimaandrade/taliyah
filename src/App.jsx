64a119da79e4c2f9670f81753a40e0142bfa980b
matheuslimaandrade <matheusla2f04@gmail.com>
2023-12-13 16:55:38 -0300

    import Hero into app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import { } from 'react'
 import './assets/styles/App.css'
 import Header from './layouts/Header/Header'
+import Hero from './layouts/Hero/Hero'
 
 function App() {
   return (
     <>
       <Header/>
+      <Hero/>
     </>
   )
 }
